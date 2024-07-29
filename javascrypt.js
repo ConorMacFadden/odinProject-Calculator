57d12db13031344f7e043948215c461af4895d78
Conor MacFadden <90274527+ConorMacFadden@users.noreply.github.com>
2024-07-29 21:29:31 +0100

    Add basic calculation and operate functions.

---
+++ javascrypt.js
@@ -0,0 +1,32 @@
+let num1;
+let num2;
+let operator;
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y ) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x/y;
+}
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case 'add':
+            return add(num1, num2);
+        case 'subtract':
+            return subtract(num1, num2);
+        case 'multiply':
+            return multiply(num1, num2);
+        case 'divide':
+            return divide(num1, num2);
+    }
+}
